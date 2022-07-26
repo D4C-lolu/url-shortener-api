81e977bb3c1662e0645c0cb0d37004b456ffac51
D4C-lolu <dareolumideisfly@gmail.com>
2022-07-26 12:20:23 +0100

    Pre-deploy commit

--- build/config/default.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: 5000,
-    dbUri: "mongodb+srv://D4C-lolu:4QDA28FUqDwcAFTx@cluster0.u0pkjgm.mongodb.net/?retryWrites=true&w=majority",
-    corsOrigin: "http://localhost:3000",
-};
--- config/production.ts
+++ config/production.ts
@@ -1,4 +1,5 @@
 export default {
   port: 5000,
-  dbUri: "mongodb://localhost:27017/url-shorterner",
+  dbUri: process.env.MONGO_URL || "mongodb://localhost:27017/url-shorterner",
+  corsOrigin: process.env.CLIENT_IP || "http://localhost:3000",
 };
